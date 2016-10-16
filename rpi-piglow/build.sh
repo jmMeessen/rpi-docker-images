4ed3e6937e8949bfbf6b2505ce2d1c3ecaef6b33
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2016-10-16 21:32:09 +0200

    working version of Piglow (cpu)

--- rpi-piglow/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -d unicorn-hat ];
-then
-   git clone https://github.com/pimoroni/piglow.git
-fi
-
-docker build -t piglow .
-
---
+++ rpi-piglow/piglow/turn-off.py
@@ -0,0 +1,9 @@
+##########################################################
+## Based on examples by Jason - @Boeeerb                ##
+##########################################################
+
+from piglow import PiGlow
+
+piglow = PiGlow()
+piglow.all(0)
+
