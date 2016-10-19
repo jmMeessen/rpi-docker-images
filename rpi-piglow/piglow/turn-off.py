f259d0418b9e8e4ef43496b3db41d7fa2f0e3d0c
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2016-10-19 21:07:42 +0200

    Create a piglow base image

--- rpi-piglow/piglow/turn-off.py
+++
@@ -1,9 +0,0 @@
-##########################################################
-## Based on examples by Jason - @Boeeerb                ##
-##########################################################
-
-from piglow import PiGlow
-
-piglow = PiGlow()
-piglow.all(0)
-
