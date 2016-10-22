f0463074b2f049f602eb1dbbc80726f7323e1b3c
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2016-10-22 21:24:25 +0200

    Implement CPU monitoring on Display-O-Tron

--- rpi-display-o-tron-cpu/display-o-tron/cpu.py
+++ rpi-display-o-tron-cpu/display-o-tron/cpu.py
@@ -21,30 +21,18 @@ class GracefulKiller:
 if __name__ == '__main__':
   killer = GracefulKiller()
   
-  # Reset the LED states and polarity
   backlight.graph_off()
   
-  # Dim the LEDs by setting the max duty to 1
   backlight.graph_set_led_duty(0, 1)
   
   while True:
     cpu = psutil.cpu_percent()
     backlight.graph_off()
 
-    if cpu < 5:
-        backlight.set_graph(1)
-    elif cpu < 20:
-        backlight.set_graph(2)
-    elif cpu < 40:
-        backlight.set_graph(3)
-    elif cpu < 60:
-        backlight.set_graph(4)
-    elif cpu < 80:
-        backlight.set_graph(5)
-    else:
-        backlight.set_graph(6)
+    backlight.set_graph(cpu /100)
+
     if killer.kill_now:
       break
-    sleep(0.2)
+    sleep(0.3)
 
 
