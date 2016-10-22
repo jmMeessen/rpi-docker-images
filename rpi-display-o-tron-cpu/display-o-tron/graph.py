02e183595fd3b6d5c0b9a10843f32e0b13c87ce1
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2016-10-22 20:01:34 +0200

    clean the CPU script

--- rpi-display-o-tron-cpu/display-o-tron/graph.py
+++ rpi-display-o-tron-cpu/display-o-tron/graph.py
@@ -1,28 +1,8 @@
 #!/usr/bin/env python
-print("""
-This example shows you how to take individual control of the bar graph LEDs.
-You should see the bar graph LEDs count up in binary!
-
-Press CTRL+C to exit.
-""")
 
 import dothat.backlight as backlight
-import dothat.lcd as lcd
+#import dothat.lcd as lcd
 import time
-
-'''
-Each LED can be either on/off,
-and brightness is controlled globally using:
-
-* graph_set_led_duty(min, max)
-
-When you don't need a bar graph, these LEDs could display
-remaining lives in a game, the status of different processes,
-the hour of the day in binary or anything else you might need!
-'''
-
-lcd.set_cursor_position(0, 1)
-lcd.write("   So Graph!    ")
 
 # Reset the LED states and polarity
 backlight.graph_off()
